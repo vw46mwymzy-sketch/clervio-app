c72abf27a4260cd780e47172896947389d4a97cf
vw46mwymzy-sketch <vw46mwymzy@privaterelay.appleid.com>
2026-07-20 10:57:53 +0200

    deploy: sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const CACHE = 'clervio-v9'
+const CACHE='clervio-v9';const ASSETS=['/',"'/index.html'","'/manifest.json'"];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(c=>c!==CACHE).map(c=>caches.delete(c)))));self.clients.claim()});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/'))))});
